61a50577f8780b583491df06e320a8d125d8ef84
wynn4 <wynnsquad@gmail.com>
2016-10-28 14:44:40 -0600

    Populated cone class files

--- cone.cpp
+++ cone.cpp
@@ -1,6 +1,66 @@
 #include "cone.h"
+#include "shape.h"
+#include<iostream>
 
-Cone::Cone()
+//Constructor
+Cone::Cone(void)
 {
 
+}
+
+//Destructor
+Cone::~Cone(void)
+{
+
+}
+
+void Cone::print()
+{
+    std::cout << "There's a cone here" << std::endl;
+}
+
+//Define the "Get" methods
+void Cone::GetRadiusX(int &rad_x)
+{
+    rad_x = mConeRadX;
+}
+
+void Cone::GetRadiusY(int &rad_y)
+{
+    rad_y = mConeRadY;
+}
+
+void Cone::GetHeight(int &height)
+{
+    height = mConeHeight;
+}
+
+void Cone::GetOrigin(int &orx, int &ory, int &orz)
+{
+    orx = mConeOrx;
+    ory = mConeOry;
+    orz = mConeOrz;
+}
+
+//Define the "Set" methods
+void Cone::SetRadiusX(const int rad_x)
+{
+    mConeRadX = rad_x;
+}
+
+void Cone::SetRadiusY(const int rad_y)
+{
+    mConeRadY = rad_y;
+}
+
+void Cone::SetHeight(const int height)
+{
+    mConeHeight = height;
+}
+
+void Cone::SetOrigin(const int orx, const int ory, const int orz)
+{
+    mConeOrx = orx;
+    mConeOry = ory;
+    mConeOrz = orz;
 }
--- cone.h
+++ cone.h
@@ -1,11 +1,34 @@
 #ifndef CONE_H
 #define CONE_H
+#include"shape.h"
 
 
-class Cone
+class Cone : public Shape
 {
 public:
-    Cone();
+    Cone(void);             //constructor
+    virtual ~Cone(void);    //destructor
+
+    void print();
+    int GetType(){return 3;}
+
+    void GetRadiusX(int &rad_x);
+    void GetRadiusY(int &rad_y);
+    void GetHeight(int &height);
+    void GetOrigin(int &orx, int &ory, int &orz);
+
+
+    void SetRadiusX(const int rad_x);
+    void SetRadiusY(const int rad_y);
+    void SetHeight(const int height);
+    void SetOrigin(const int orx, const int ory, const int orz);
+
+
+private:
+    int mConeRadX;
+    int mConeRadY;
+    int mConeHeight;
+    int mConeOrx, mConeOry, mConeOrz;
 };
 
 #endif // CONE_H
